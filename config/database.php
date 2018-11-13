96a2f65f74b006a3e48f2ac2af60219ae68179a2
dyatmika12 <dyatmika12@gmail.com>
2018-11-13 16:57:57 +0800

    detobese

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel_prognet'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'gcp_c4358fe34284acef2d95'),
+            'username' => env('DB_USERNAME', 'b78bd0f256cd4d'),
+            'password' => env('DB_PASSWORD', '204da221'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
